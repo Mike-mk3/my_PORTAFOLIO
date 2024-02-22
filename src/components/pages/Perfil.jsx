4144f4f661591abdd8d4f5dbac108c465c80f34d
Mike-mk3 <mike666mtz@gmail.com>
2024-02-22 14:42:34 -0600

    paso 1
    Agregue algo de mi informacion de contacto

--- src/components/pages/Perfil.jsx
+++ src/components/pages/Perfil.jsx
@@ -2,6 +2,54 @@ function Perfil() {
     return ( 
         <div className="container page">
             <h1 className="text-center mb-4">PERFIL</h1>
+            <br />
+
+
+
+
+
+
+
+
+
+
+
+
+
+<h2>Contactame En:</h2>
+<table className="py-4 d-flex   bg-body-secondary  flex-column align-items-center">
+
+  <tbody>
+    <tr>
+
+    </tr>
+    <tr>
+      <th >E-Mail:</th>
+      <td>Mike666mtz@gmail.com</td>
+
+    </tr>
+    <tr>
+      <th >Facebook:</th>
+      <td>Mike Martinez</td>
+
+    </tr>
+    <tr>
+      <th >Instagram:</th>
+      <td>Mike__mk</td>
+
+    </tr>
+    <tr>
+      <th >Linkedin:</th>
+      <td >Miguel Ángel Saldaña Martinez</td>
+
+    </tr>
+    <tr>
+      <th >Git Hub:</th>
+      <td>Mike-mk3</td>
+      </tr>
+  </tbody>
+</table>
+
         </div>
     );
 }
