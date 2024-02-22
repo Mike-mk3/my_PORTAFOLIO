c0fabff45f9f98af4048c036c48ee22d4e7db2a6
Mike-mk3 <mike666mtz@gmail.com>
2024-02-22 00:06:12 -0600

    pages
    Agregado de las paginas que ocupare

---
+++ src/components/pages/Perfil.jsx
@@ -0,0 +1,5 @@
+function Perfil() {
+    return ( <h1>_mi self_</h1> );
+}
+
+export default Perfil;
---
+++ src/components/pages/Proyectos.jsx
@@ -0,0 +1,5 @@
+function Perfil() {
+    return ( <h1>_mis pryyectos_</h1> );
+}
+
+export default Perfil;
