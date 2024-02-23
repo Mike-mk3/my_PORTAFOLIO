9c9300791012e66fa2a44088dc008827ff528f9a
Mike-mk3 <mike666mtz@gmail.com>
2024-02-23 01:01:02 -0600

    pagina 2
    elaboracion del apartado de proyectos

--- src/components/pages/Proyectos.jsx
+++ src/components/pages/Proyectos.jsx
@@ -9,33 +9,127 @@ function Perfil() {
                     <img src="https://www.3presupuestos.com/fotos/wp-content/uploads/2016/04/Restaurante-cla%CC%81sico-con-suelo-de-gres-973x649.jpg" className="w-100" alt="" />
                 </div>
                 <div className="col-md-6 p-4 ps-md-0">
-                    <h2 className="mt-0 text-center">= Frase del Dia =</h2 >
-                    <p className="mt-0 text-center">
-                        "Algo más valioso que todas las riquezas que hay sobre la tierra, es tomar una copa de vino en el momento preciso"
+                    <h2 className="mt-0 text-center">= Lading De Venta =</h2 >
+
+
+
+                    <p className="mt-0 text-justifyc">
+                        Proyecto realizado para la venta de una Motocicleta. <th></th>
+                        Header con menu con botones con rutas hacia informacion de la compañia asi como del producto en venta. <th></th>
+                        Espacio en la pagina con informacion del producto, la opcion de registro de usuario a la pagina y un espacio para agendar cita o hacer la compra en linea asi como tambien imagenes y fotos del producto. <th />
+                        Footer con un apartado con nuestras redes sociales y asesores disponibles para contactar.
                     </p>
+                    <a href="https://github.com/Mike-mk3/pro1" className="btn btn-primary">Repository</a>
+                    <th></th>
+                    <a href="https://main--gregarious-meringue-ea6798.netlify.app/" className="btn btn-primary">On-Line</a>
                 </div>
             </div>
             <br />
 
 
+            <br />
+            <div className="row g-0 bg-body-secondary position-relative">
+                <div className="col-md-6 mb-md-0 p-md-4">
+                    <img src="https://www.3presupuestos.com/fotos/wp-content/uploads/2016/04/Restaurante-cla%CC%81sico-con-suelo-de-gres-973x649.jpg" className="w-100" alt="" />
+                </div>
+                <div className="col-md-6 p-4 ps-md-0">
+                    <h2 className="mt-0 text-center">= App CRUD =</h2 >
 
 
 
+                    <p className="mt-0 text-justifyc">
+                        Proyecto realizado para la venta de una Motocicleta. <th></th>
+                        Header con menu con botones con rutas hacia informacion de la compañia asi como del producto en venta. <th></th>
+                        Espacio en la pagina con informacion del producto, la opcion de registro de usuario a la pagina y un espacio para agendar cita o hacer la compra en linea asi como tambien imagenes y fotos del producto. <th />
+                        Footer con un apartado con nuestras redes sociales y asesores disponibles para contactar.
+                    </p>
+                    <a href="https://github.com/Mike-mk3/PROYECTO_2.git" className="btn btn-primary">Repository</a>
+                    <th></th>
+                    <a href="https://dazzling-cheesecake-ce8f08.netlify.app" className="btn btn-primary">On-Line</a>
+                </div>
+            </div>
+            <br />
+
+
+            <br />
+            <div className="row g-0 bg-body-secondary position-relative">
+                <div className="col-md-6 mb-md-0 p-md-4">
+                    <img src="https://www.3presupuestos.com/fotos/wp-content/uploads/2016/04/Restaurante-cla%CC%81sico-con-suelo-de-gres-973x649.jpg" className="w-100" alt="" />
+                </div>
+                <div className="col-md-6 p-4 ps-md-0">
+                    <h2 className="mt-0 text-center">= Dashboard App =</h2 >
+
+
+
+                    <p className="mt-0 text-justifyc">
+                        Proyecto realizado para la venta de una Motocicleta. <th></th>
+                        Header con menu con botones con rutas hacia informacion de la compañia asi como del producto en venta. <th></th>
+                        Espacio en la pagina con informacion del producto, la opcion de registro de usuario a la pagina y un espacio para agendar cita o hacer la compra en linea asi como tambien imagenes y fotos del producto. <th />
+                        Footer con un apartado con nuestras redes sociales y asesores disponibles para contactar.
+                    </p>
+                    <a href="https://github.com/Mike-mk3/PROYECTO-3" className="btn btn-primary">Repository</a>
+                    <th></th>
+                    <a href="https://magenta-eclair-f95b94.netlify.app/" className="btn btn-primary">On-Line</a>
+                </div>
+            </div>
+            <br />
+
+
+            <br />
+            <div className="row g-0 bg-body-secondary position-relative">
+                <div className="col-md-6 mb-md-0 p-md-4">
+                    <img src="https://www.3presupuestos.com/fotos/wp-content/uploads/2016/04/Restaurante-cla%CC%81sico-con-suelo-de-gres-973x649.jpg" className="w-100" alt="" />
+                </div>
+                <div className="col-md-6 p-4 ps-md-0">
+                    <h2 className="mt-0 text-center">= Restaurant App =</h2 >
+
+
+
+                    <p className="mt-0 text-justifyc">
+                        Proyecto realizado para la venta de una Motocicleta. <th></th>
+                        Header con menu con botones con rutas hacia informacion de la compañia asi como del producto en venta. <th></th>
+                        Espacio en la pagina con informacion del producto, la opcion de registro de usuario a la pagina y un espacio para agendar cita o hacer la compra en linea asi como tambien imagenes y fotos del producto. <th />
+                        Footer con un apartado con nuestras redes sociales y asesores disponibles para contactar.
+                    </p>
+                    <a href="https://github.com/Mike-mk3/clase-15" className="btn btn-primary">Repository</a>
+                    <th></th>
+                    <a href="https://beamish-conkies-5b4a56.netlify.app/" className="btn btn-primary">On-Line</a>
+                </div>
+            </div>
+            <br />
+
+
+            <br />
+            <div className="row g-0 bg-body-secondary position-relative">
+                <div className="col-md-6 mb-md-0 p-md-4">
+                    <img src="https://www.3presupuestos.com/fotos/wp-content/uploads/2016/04/Restaurante-cla%CC%81sico-con-suelo-de-gres-973x649.jpg" className="w-100" alt="" />
+                </div>
+                <div className="col-md-6 p-4 ps-md-0">
+                    <h2 className="mt-0 text-center">= Comercio Electronico =</h2 >
+
+
+
+                    <p className="mt-0 text-justifyc">
+                        Proyecto realizado para la venta de una Motocicleta. <th></th>
+                        Header con menu con botones con rutas hacia informacion de la compañia asi como del producto en venta. <th></th>
+                        Espacio en la pagina con informacion del producto, la opcion de registro de usuario a la pagina y un espacio para agendar cita o hacer la compra en linea asi como tambien imagenes y fotos del producto. <th />
+                        Footer con un apartado con nuestras redes sociales y asesores disponibles para contactar.
+                    </p>
+                    <a href="https://github.com/Mike-mk3/semifinal_front" className="btn btn-primary">Repository FrontEnd</a>
+                    <th></th>
+                    <a href="https://github.com/Mike-mk3/API_ECOMMERCE" className="btn btn-primary">Repository BackEnd</a>
+                    <th></th>
+                    <a href="https://ecommercemike.netlify.app" className="btn btn-primary">On-Line</a>
+                </div>
+            </div>
+            <br />
+        
+        
+        
+        
+        
+        
         </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     );
 }
 
